39872bdd57c2f5a2bd0a8cfbd0795ed4f7a831e3
ArthurBonsu <peeman34@gmail.com>
2021-03-15 20:14:22 +0800

    MediationNegotiation.sol

--- MediationNegotiation.sol
+++ MediationNegotiation.sol
@@ -40,7 +40,58 @@ MediationStructureIntelligent  mediationstructurenode;
 
 event mediationBidEvent (address mediationid, string  mediationname, uint mediationphase,  uint  mediationparticipantcount, uint mediationcount,address payable participantaddress, string participantname,uint offermade);
 
-function MediationBid (address mediationid, string memory mediationname, uint mediationphase,  uint  mediationparticipantcount, uint mediationcount,address payable participantaddress, string memory participantname,uint offermade)  internal {
+function MediationBid (address mediationid, string memory mediationname, uint mediationphase,  uint  mediationparticipantcount, uint mediationcount,address payable participantaddress, string memory participantname,uint offermade)  public returns(address _mediationid, string memory _mediationname, uint _mediationphase,  uint  _mediationparticipantcount, uint _mediationcount,address payable _participantaddress, string memory _participantname,uint _offermade) {
+       uint i = 0;
+           bidallow = true;
+           if(bidallow == true){
+               
+    
+    
+               
+     mediationstructurenode.mediationstructureaddressedmap[participantaddress][i].mediationid =mediationid;
+     mediationstructurenode.mediationstructureaddressedmap[participantaddress][i].mediationname = mediationname;
+       mediationstructurenode.mediationstructureaddressedmap[participantaddress][i].mediationphase = mediationphase;
+     mediationstructurenode.mediationstructureaddressedmap[participantaddress][i].mediationparticipantcount =mediationparticipantcount;
+     mediationstructurenode.mediationstructureaddressedmap[participantaddress][i].mediationcount = i;
+     mediationstructurenode.mediationstructureaddressedmap[participantaddress][i].participantaddress =participantaddress;
+     mediationstructurenode.mediationstructureaddressedmap[participantaddress][i].participantname = participantname;
+     mediationstructurenode.mediationstructureaddressedmap[participantaddress][i].offermade =offermade;
+      mediationstructurenode.mediationstructureaddressedmap[participantaddress][i].previousoffer =0;
+     
+// NO PREVIOUS OFFER IN THIS WORK HERE
+
+
+emit mediationBidEvent( mediationid, mediationname, mediationphase,  mediationparticipantcount,  mediationcount,  participantaddress,  participantname, offermade);
+
+
+ 
+ mediationstructurestore.push(MediationStructureIntelligent(
+     {
+                   
+   mediationid:  mediationstructurenode.mediationstructureaddressedmap[participantaddress][i].mediationid,
+   mediationname:  mediationstructurenode.mediationstructureaddressedmap[participantaddress][i].mediationname,
+   mediationphase:    mediationstructurenode.mediationstructureaddressedmap[participantaddress][i].mediationphase,
+   mediationparticipantcount:  mediationstructurenode.mediationstructureaddressedmap[participantaddress][i].mediationparticipantcount,
+   mediationcount:  mediationstructurenode.mediationstructureaddressedmap[participantaddress][i].mediationcount,
+   participantaddress:   mediationstructurenode.mediationstructureaddressedmap[participantaddress][i].participantaddress,
+   participantname:   mediationstructurenode.mediationstructureaddressedmap[participantaddress][i].participantname,
+   offermade:  mediationstructurenode.mediationstructureaddressedmap[participantaddress][i].offermade,
+   previousoffer:   mediationstructurenode.mediationstructureaddressedmap[participantaddress][i].previousoffer
+   
+                           
+ })); 
+ 
+ 
+   }
+
+     i++;
+         returns ( mediationid, mediationname, mediationphase,  mediationparticipantcount,  mediationcount,participantaddress, participantname, offermade)
+  
+  
+} 
+     
+     
+function ReMediationBid (address mediationid, string memory mediationname, uint mediationphase,  uint  mediationparticipantcount, uint mediationcount,address payable participantaddress, string memory participantname,uint offermade)  internal {
        uint i = 0;
            bidallow = true;
            if(bidallow == true){
@@ -91,6 +142,5 @@ emit mediationBidEvent( mediationid, mediationname, mediationphase,  mediationpa
 } 
      
      
-
 
 }
