ec0e4f4fb3fbf9fb1e370f7e87fe6c2e253b90e0
ArthurBonsu <peeman34@gmail.com>
2021-03-18 03:07:37 +0800

    Arbitration.sol
    Just a simple arbitration contract

--- Arbitration.sol
+++ Arbitration.sol
@@ -32,8 +32,8 @@ contract ArbitrationContract{
         Arbitrant arbitrantnode;
         
          struct Defendants{
-             address arbitrantaddress;
-             uint256 arbitrantid;
+             address defendantaddress;
+             uint256 defendantid;
                mapping (address => Defendants[1000]) defendantaddressset;
       mapping (bytes32 => Defendants[1000]) defendantbytesset;
 
@@ -45,8 +45,8 @@ contract ArbitrationContract{
         
         
          struct Claimant{
-             address arbitrantaddress;
-             uint256 arbitrantid;
+             address claimantaddress;
+             uint256 claimantid;
                 mapping (address => Claimant[1000]) claimantaddressset;
       mapping (bytes32 => Claimant[1000]) claimantbytesset;
 
@@ -60,9 +60,9 @@ contract ArbitrationContract{
              address arbitrantaddress;
              address defendantaddress;
              address claimantaddress;
+             bytes32 disputetime;   
                
-               
-                 mapping (address => Dispute[1000]) disputeaddressset;
+      mapping (address => Dispute[1000]) disputeaddressset;
       mapping (bytes32 => Dispute[1000]) disputebytesset;
 
        }
@@ -70,34 +70,72 @@ contract ArbitrationContract{
         Dispute[] public disputestore; 
         Dispute disputenode;
   
-  
-  function arbitrators(address _arbitrationaddress, uint256 _disputeid) public pure returns (address _arbitrationaddres, uint256 _arbitrationdisputeid){
+     // Enteries or Writes
+  function arbitrators(address arbitrationaddress, uint256 arbitrationid, uint256 _disputeid) public payable returns (address _arbitrationaddres, uint256 _arbitrationid){
                              
-       arbitrantnode.arbitrantstore[_arbitrationaddress][_disputeid].arbitrantaddress = _arbitrationaddress;
-         arbitrantnode.arbitrantstore[_arbitrationaddress][_disputeid].arbitrantid = arbitrantid;
-       return ( _arbitrationaddress, _disputeid);
+       arbitrantnode.arbitrantaddressset[arbitrationaddress][_disputeid].arbitrantaddress = arbitrationaddress;
+         arbitrantnode.arbitrantaddressset[arbitrationaddress][_disputeid].arbitrantid = arbitrationid;
+       return ( arbitrationaddress, arbitrationid);
+                                 
+  }
+  function defendants(address defendantaddress, uint256 _defendantid, uint256 _disputeid) public payable returns (address __defendantaddress, uint256 edefendantid){
+                             
+       defendantnode.defendantaddressset[defendantaddress][_disputeid].defendantaddress = defendantaddress;
+         defendantnode.defendantaddressset[defendantaddress][_disputeid].defendantid = _defendantid;
+       return ( defendantaddress,  _defendantid);
                                  
   }
 
-  function disputeClaimant(address claimantaddress,  uint256 _disputeId) external view returns (address claimantaddress,  uint256 _disputeId){
-                 claimantnode.claimantstore [_arbitrationaddress][_disputeid];                 
-       
-       
-       return ();
-      
-  };
-  function disputeArbitrable(uint256 _disputeId
-    ) external view returns (address);
-  function disputeDefendant(uint256 _disputeId
-    ) external view returns (address);
+  function claimants(address claimantaddress, uint256 _claimid, uint256 _disputeid) public payable returns (address _claimantaddress, uint256 eclaimantid){
+                             
+       claimantnode.claimantaddressset[claimantaddress][_disputeid].claimantaddress = claimantaddress;
+         claimantnode.claimantaddressset[claimantaddress][_disputeid].claimantid = _claimid;
+       return ( claimantaddress,_claimid);
+                                 
+  }
+
+/*
+ 
   function disputeRaisedAt(uint256 _disputeId) external view returns (uint256);
   function disputeResolutionHash(uint256 _disputeId
     ) external view returns (bytes32){
-        bytes32 disputehash  = keccak256(disputeId)
-    };
+        bytes32 disputehash  = keccak256(disputeId);
+    }
   function disputeStatus(uint256 _disputeId
     ) external view returns (DisputeStatus);
 
+*/
+
+// Dispute Reads
+
+  function disputeClaimant(address claimantaddress,  uint256 _disputeId) public view returns (address _claimantaddress,  uint256 thegreatdisputeId){
+             address theclaimant =      claimantnode.claimantaddressset [claimantaddress][_disputeId].claimantaddress;
+              uint disputeid =        claimantnode.claimantaddressset [claimantaddress][_disputeId].claimantid;    
+       
+       
+       return (theclaimant,disputeid );
+      
+  }
+
+  function disputeArbitrable(address arbitrantaddress, uint256 _disputeId) public view returns (address _arbitrantaddress, uint256 thegreatdisputeId){
+             address thearbitration =      arbitrantnode.arbitrantaddressset [arbitrantaddress][_disputeId].arbitrantaddress;
+              uint disputeid =        arbitrantnode.arbitrantaddressset [arbitrantaddress][_disputeId].arbitrantid;    
+       
+       
+       return (thearbitration,disputeid );
+                 
+        
+    }
+
+ function disputeDefendant(address defendantaddress, uint256 _disputeId) public view  returns (address _defendantaddress, uint256 thegreatdisputeId){
+      
+             address thedefendants =      defendantnode.defendantaddressset [defendantaddress][_disputeId].defendantaddress;
+              uint thedefendantsid =        defendantnode.defendantaddressset [defendantaddress][_disputeId].defendantid;    
+      
+       return(defendantaddress, _disputeId);
+  }
+
+/*
   function disputesCount() external view returns (uint256);
 
   function raiseDispute(
@@ -128,9 +166,10 @@ contract ArbitrationContract{
     uint256 disputeId, address indexed author, bytes32 resolutionHash);
 
   event DisputeInterrupted(uint256 disputeId, address indexed author);
-  event DisputeResumed(uint256 disputeId, address indexed author);
+ event DisputeResumed(uint256 disputeId, address indexed author);
 
   event Repudiated(bytes reason);
+
+    */
 }
 
-}}
